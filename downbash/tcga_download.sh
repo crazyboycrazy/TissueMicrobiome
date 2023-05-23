413423012cb441845e3c8962e11fdbf4fb4a4100
crazyboycrazy <icecap3954@gm.gist.ac.kr>
2023-05-23 20:56:09 +0900

    microbiome

--- downbash/tcga_download.sh
+++ downbash/tcga_download.sh
@@ -7,5 +7,5 @@ first=$1
 second=$2
 for i in $(cat manifest|grep -v filename|cut -f 1 | sed -n "${first},${second}p")
 do echo $i
-gdc-client download -t /home/junwoojo/token $i
+gdc-client download --no-annotations --no-related-files -t /home/junwoojo/token $i
 done
