fe424fcc12dc8fc5e693c91cbcec1afb9442e9eb
crazyboycrazy <89303283+crazyboycrazy@users.noreply.github.com>
2023-06-15 16:13:03 +0900

    Create Metawrap_work.sh

---
+++ Metawrap/process_file/Metawrap_work.sh
@@ -0,0 +1,18 @@
+#merge fastq
+cat */*_1.fastq > merged_1.fastq 
+cat */*_2.fastq > merged_2.fastq 
+
+# assembly
+megahit -t 11 -1 merged_1.fastq -2 merged_2.fastq -o megahit_out
+
+# binning
+metawrap binning -a output_megahit/final.contigs.fa -o metawrap_out -t 9 -m 100 --metabat2 --maxbin2 --concoct */*fastq
+
+# refinement
+metawrap bin_refinement -o output_refinement -t 9 -m 80 -A concoct_bins/ -B maxbin2_bins/ -C metabat2_bins/
+
+#calssify (GTDBTK)
+
+
+#metawrap quant
+metawrap quant_bins -b outut_metarwrap/output_refinement/metawrap_70_10_bins/ -a  output_megahit/final.contigs.fa -o quant_output   -t 9 *TCGA*/*fastq
